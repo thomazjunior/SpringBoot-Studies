2279fa8a577609b1018a383fef912cd31e0f1082
thomazjunior <p.thomaz.junior@gmail.com>
2023-04-08 21:36:34 +0100

    security filter chain implemented and spring boot and java version altered

--- spring-security/src/test/java/com/springboot/studies/springsecurity/HomeControllerTest.java
+++ spring-security/src/test/java/com/springboot/studies/springsecurity/HomeControllerTest.java
@@ -1,24 +1,49 @@
 package com.springboot.studies.springsecurity;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest(classes = SpringSecurityApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HomeControllerTest {
 
+    // @Autowired
+    // private TestRestTemplate template;
+
     @Autowired
-    private TestRestTemplate template;
+    private MockMvc mockMvc;
+
+    // @Test
+    // public void hello_ok() throws Exception {
+    // ResponseEntity<String> response = template.withBasicAuth("teste",
+    // "password").getForEntity("/", String.class);
+    // assertThat(response.getBody()).isEqualTo(null);
+    // }
 
     @Test
-    public void hello_ok() throws Exception {
-        ResponseEntity<String> response = template.withBasicAuth("teste", "password").getForEntity("/", String.class);
-        assertThat(response.getBody()).isEqualTo(null);
+    @DisplayName("Should authenticate user")
+    @WithMockUser
+    void helloOk() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello world user"));
     }
 
+    @Test
+    @DisplayName("Should fail to authenticate user")
+    void helloUnauthorized() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isUnauthorized());
+    }
 }
